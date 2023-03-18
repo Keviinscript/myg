5baabe2a5f64a71c14d98435c6bc232e17d246f5
ikechukwu kevin-chika <101973303+Keviinscript@users.noreply.github.com>
2023-03-18 20:52:54 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+   
+    extend: {
+      colors: {
+        'mainblue': 'rgb(91,209,215)',
+        'mainbluee': 'rgb(188,206,251)',
+      },
+    },
+  },
+  plugins: [],
+}
